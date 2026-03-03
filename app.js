c681dbc5c4ade6b0e4fe6560430ccb738b543273
Bharath Naik <bharathnaik8197@gmail.com>
2026-03-03 15:07:30 +0530

    Update response message in root route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps Project Running 🚀");
+  res.send("Hey Bharath DevOps Project Running 🚀");
 });
 
 app.listen(3000, () => {
