10b9c09fedd114d22bed6860243252e110f63cc4
Bharath Naik <bharathnaik8197@gmail.com>
2026-03-03 15:07:52 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hey Bharath DevOps Project Running 🚀");
+  res.send("Hey Bharath DevOps Your Project Running 🚀");
 });
 
 app.listen(3000, () => {
